8b494f0ae093fedf39fe2a32b17f53af618268d5
Zack Dever <zackdever@gmail.com>
2012-09-13 18:19:48 -0700

    more tests

--- test/time.test.js
+++ test/time.test.js
@@ -6,6 +6,54 @@ for (var i = 1; i < 13; i++) hours.push(i.toString());
 for (var i = 0; i < 60; i++) minutes.push(('0' + i).slice(-2));
 
 describe('Time', function() {
+  describe('constructor', function() {
+    it('should work without using `new`', function() {
+      var t = time();
+      t.nextDate().should.be.ok;
+      t = time(3);
+      t.hours.should.equal(3);
+      t.minutes.should.equal(0);
+    });
+
+    it('should work using `new`', function() {
+      var t = new time();
+      t.nextDate().should.be.ok;
+      t = time(3);
+      t.hours.should.equal(3);
+      t.minutes.should.equal(0);
+    });
+
+    it('should be the same with or without `new`', function() {
+      var newT = new time('314')
+        , t = time('314');
+      t.nextDate().should.equal(newT.nextDate());
+      t.hours.should.equal(newT.hours);
+      t.minutes.should.equal(newT.minutes);
+    });
+  });
+
+  describe('(static) #isValid', function() {
+    it('should pass all the hours 1-12', function() {
+      var hour, result;
+      for (var i = 0; i < hours.length; i++) {
+        hour = hours[i];
+        time.isValid(hour).should.be.ok;
+      }
+    });
+
+    it('should pass all the hours with all the minutes 1:00 - 12:59', function() {
+      var hour, minute, result, input;
+      for (var i = 0; i < hours.length; i++) {
+        for (var j = 0; j < minutes.length; j++) {
+          hour = hours[i];
+          minute = minutes[j];
+          input = hour + ':' + minute;
+          time.isValid(input).should.be.ok;
+        }
+      };
+    });
+  });
+
   describe('#isValid', function() {
     it('should pass all the hours 1-12', function() {
       var hour, result;
@@ -57,6 +105,16 @@ describe('Time', function() {
           result.minutes.should.equal(parseInt(minute));
         }
       }
+    });
+
+    it('should pass otherwise valid times with arbitrary whitespace', function() {
+      time('  130').isValid().should.be.ok;
+      time('  1   3  0   ').isValid().should.be.ok;
+      time('1 . 30   ').isValid().should.be.ok;
+      time.isValid(' 7   ').should.be.ok;
+      time.isValid('   12:  30   ').should.be.ok;
+      time.isValid('   9    ').should.be.ok;
+      time.isValid('   9   . 00   ').should.be.ok;
     });
 
     it('should fail made up minutes e.g. 1:3, 1:60, 1:122', function() {
